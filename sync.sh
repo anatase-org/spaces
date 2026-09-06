91ddf315dc59cfd4e01562eff089e035187d6abd
Antheas Kapenekakis <git@antheas.dev>
2026-09-06 03:50:09 +0200

    add missing and

--- sync.sh
+++ sync.sh
@@ -144,7 +144,7 @@ scp "${rpm_paths[@]}" "$remote_host:"
 
 echo "Installing ${rpm_names[*]} on $remote_host..."
 ssh -t "$remote_host" "sudo rpm-ostree usroverlay || true
-sudo systemctl stop 'spaces@*'
+sudo systemctl stop 'spaces@*' &&
 sudo dnf5 install -y $remote_rpms &&
 sudo systemctl try-reload-or-restart polkit.service"
 
